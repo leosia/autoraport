484c35a829f6537c49c2e77743ed2690fdc1f55d
leosia <sobekx@gmail.com>
2017-07-18 15:31:24 +0200

    GUI - almost video processing - done pdfcreator - done

--- auto.py
+++ auto.py
@@ -1,18 +1,52 @@
 # -*- coding: UTF-8 -*-
 
 import kivy
+import os
+import video
 import numpy as np
+import getpass
+from shutil import copyfile
 
 from kivy.app import App
+from kivy.uix.popup import Popup
+from kivy.uix.progressbar import ProgressBar
+from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 
+from kivy.config import Config
+kivy.config.Config.set('graphics','resizable', False)
+
+class VideoPopup(Popup):
+
+    filechooser = ObjectProperty
+
+    def __init__(self, *args, **kwargs):
+        super(VideoPopup, self).__init__(**kwargs)
+        self.filechooser.path = r'C:\Users' + '\\' + getpass.getuser() + '\\' + 'Desktop'
 
 
+    def copy_files(self, filepath, selection, text):
+        print(filepath, selection, text)
+
+        # fileList = [os.path.normcase(f) for f in os.listdir(directory)]
+        # fileList = [f for f in fileList if os.path.splitext(f)[1] in ext_list]
+        # return fileList
 
 class AutoLayout(BoxLayout):
+
+    fps = ObjectProperty
+    start = ObjectProperty
+    interval = ObjectProperty
+    vid_path = ObjectProperty
+    tu_num = ObjectProperty
+
     def __init__(self, *args, **kwargs):
         super(AutoLayout, self).__init__(**kwargs)
+        self.load_popup = VideoPopup()
 
+    def video_processing(self):
+        vid = video.Video(self.fps, self.start, self.interval, self.vid_path, self.to_num)
+        vid.main()
 
 class AutoApp(App):
     def __init__(self, **kwargs):
--- video.py
+++ video.py
@@ -2,23 +2,49 @@
 
 import cv2
 import os
+import re
+import getpass
+import numpy as np
+import datetime as dt
+from shutil import copyfile
+
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import cm
-from shutil import copyfile
-import numpy as np
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import cm
+from reportlab.pdfbase.pdfmetrics import stringWidth
+from reportlab.lib.pagesizes import A4
+
 
 class Video:
+    def __init__(self, fps, start, interval, vid_path="", to_num="TO-XXXXXXXX"):
+        self.fps = fps
+        self.start = start
+        self.to_num = to_num
+        self.interval = interval
+        self.vid_path = vid_path
+        self.temp_dict = r'C:\Temp'
+        self.ext_list = ['.avi', '.mp4', '.mkv']
 
-    def file_list(self, directory, file_ext):
+
+    def main(self):
+        self.temp_dirs(self.temp_dict, self.ext_list)
+        file_list = self.file_list(self.temp_dict, self.ext_list)
+        for file in file_list:
+            self.video_cutter(file, self.temp_dict)
+        self.pdf_creator()
+
+
+    def file_list(self, directory, ext_list):
         """
-        Funkcja przygotowuje listę plików w konkretnym katalogu, mających rozszerzenie pasujące do
+        Funkcja przygotowuje listę plików konkretnego katalogu, mających rozszerzenie pasujące do
         podanej listy rozszerzeń.
         :param directory: Folder docelowy
         :param file_ext: lista rozszerzeń plików
         :return: lista plików (lowercase na wszystkie nazwy)
         """
         fileList = [os.path.normcase(f) for f in os.listdir(directory)]
-        fileList = [f for f in fileList if os.path.splitext(f)[1] in file_ext]
+        fileList = [f for f in fileList if os.path.splitext(f)[1] in ext_list]
         return fileList
 
 
@@ -35,6 +61,11 @@ class Video:
                 os.mkdir(temp_dict)
 
 
+    def time_converter(self, frame):
+        time = (1000/3000)*(frame - 6)
+        return time
+
+
     def video_cutter(self, filename, path):
         """
         Funkcja rozbija plik avi na klatki i zapisuje każdą pod nazwą imgXXX.jpg (gdzie XXX to kolejna liczba)
@@ -44,48 +75,96 @@ class Video:
         """
         file = path + "\\" + filename
         video = cv2.VideoCapture(file)
-        counter = 0
-        #TODO; sprawdzić dlaczego tworzy tylko jedną klatke
+        c = 0
         while(video.isOpened()):
             ret, frame = video.read()
-            print(ret)
-            img_name = filename[:11] + str(counter) + '.jpg'
-            img_path = r'C:\Temp' + "\\" + filename[:-4] + "\\" + img_name
-            if ret == True:
-                cv2.imwrite(img_path, frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+            if self.time_converter(c) % self.interval == 0:
+                img_name = filename[:11] + str(c) + "_" + format(self.time_converter(c), '.1f') + '.jpg'
+                img_path = r'C:\Temp' + "\\" + filename[:-4] + "\\" + img_name
+                if ret == True:
+                    cv2.imwrite(img_path, frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                else:
                     break
-            else:
-                break
-            counter += 1
+            c += 1
         video.release()
-        # cv2.destroyAllWindows()
+
 
     def pdf_creator(self):
-        pass
 
-    def main(self):
-        """
+        def sorted_nicely(l):
+            """
+            # http://stackoverflow.com/questions/2669059/how-to-sort-alpha-numeric-set-in-python
+            Sort the given iterable in the way that humans expect.
+            """
+            convert = lambda text: int(text) if text.isdigit() else text
+            alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+            return sorted(l, key=alphanum_key)
 
-        :return: None
-        """
-        main_path = r'\\APL-FS06\Laboratory\02_Operations\00_COP\03_Auto_Reports_Fiat'
-        temp_dict = r'C:\Temp'
-        ext_list = ['.avi']
-        # self.temp_dirs(main_path, ext_list)
-        # file_list = self.file_list(main_path, ext_list)
-        # for file in file_list:
-        #     src = main_path + "\\" + file
-        #     dst = r'C:\Temp' + "\\" + file
-        #     copyfile(src, dst)
-        #     self.video_cutter(file, temp_dict)
-        self.temp_dirs(temp_dict, ext_list)
-        file_list = self.file_list(temp_dict, ext_list)
-        for file in file_list:
-            src = main_path + "\\" + file
-            dst = r'C:\Temp' + "\\" + file
-            # copyfile(src, dst)
-            self.video_cutter(file, temp_dict)
+        def prepare_description(filename):
+            name = os.path.splitext(filename)
+            time = name[0][-4:]
+            if time[:1] == '_':
+                time = time[1:]
+            text = name[0][:10].upper() + '   Time: ' + time + ' ms'
+            return text
 
-v = Video()
+        date = dt.datetime.now()
+        fileList = [f for f in os.listdir(self.temp_dict) if os.path.isfile(self.temp_dict + "\\" + f)]
+        dirList = [f for f in os.listdir(self.temp_dict) if not os.path.isfile(self.temp_dict + "\\" + f)]
+
+        file_list_sorted = []
+        for file_no in range(0, len(fileList)):
+            file_list_sorted.append(sorted_nicely(os.listdir(self.temp_dict + "\\" + dirList[file_no])))
+
+        files_qty = len(sorted_nicely(os.listdir(self.temp_dict + "\\" + dirList[0])))
+
+        username = getpass.getuser().split('.')
+        username = username[0].capitalize() + ' ' + username[1].capitalize()
+
+        doc = canvas.Canvas('report.pdf', pagesize=A4)
+
+        doc.setFont('Helvetica-Bold', 65)
+        doc.setFillColorRGB(0, (40/255), (81/255))
+        doc.drawCentredString((A4[0] / 2), ((A4[1] / 2) + 5 * cm), "Fiat Report")
+        doc.setFontSize(24)
+        doc.drawImage('logo2.png', ((A4[0] / 2)-((A4[0]/3)/2.125)), ((A4[1] / 2)), A4[0]/3, (A4[0]/3)/2.125, preserveAspectRatio=True, anchor='c')
+        doc.drawCentredString((A4[0] / 2), ((A4[1] / 2) - 1 * cm), self.to_num)
+        doc.drawCentredString((A4[0] / 2), ((A4[1] / 2) - 5 * cm), 'Created:')
+        doc.drawCentredString((A4[0] / 2), ((A4[1] / 2) - 6 * cm), date.strftime('%d %B %Y'))
+        doc.drawCentredString((A4[0] / 2), ((A4[1] / 2) - 7 * cm), username)
+        doc.showPage()
+
+        columns = len(dirList)
+        margins = 0.5 * cm
+        spacing = 0.5 * cm
+
+        box_width = (A4[0] - 2 * margins - (spacing * (columns - 1))) / columns
+        box_height = box_width / 0.8
+
+        pos_y = box_height * (files_qty - 1) + margins
+
+        doc.setPageSize((A4[0], files_qty * box_height + (2 * cm)))
+        doc.setFont('Helvetica-Bold', 38 / (columns if columns < 12 else columns * 2))
+
+
+        for y in range(0, files_qty):
+            for x in range(0, columns):
+                pos_x = margins + x * (box_width + margins)
+                src = self.temp_dict + '\\' + dirList[x] + '\\' + file_list_sorted[x][y]
+                doc.drawImage(src, pos_x, pos_y, box_width, box_height, preserveAspectRatio=True, anchor='n')
+                doc.drawCentredString(pos_x + (box_width / 2), pos_y + ((box_height - box_width) / 2), prepare_description(file_list_sorted[x][y]))
+                if y == files_qty and x == columns:
+                    break
+            if y == files_qty:
+                break
+            pos_y -= box_height
+
+        doc.showPage()
+        doc.save()
+
+        return True
+
+v = Video(3000, 20, 15)
 v.main()
